c11760afbb896a7468a5c9758f8809e3b892860f
Marko Yakovenko <yakovenkomarko@gmail.com>
2025-06-24 21:58:16 +0200

    Added essential packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import './assets/styles/reset.css'
+import './assets/styles/global-variables.css'
+import './assets/styles/fonts.css'
+import StartScreen from '@/src/components/StartScreen/StartScreen'
+import classes from "./App.module.css"
+import { useState } from 'react'
+import type { TGameMode } from './components/types'
+import HearAndTypeScreen from './components/GameScreen/HearAndTypeScreen'
+import SeeAndTypeScreen from './components/GameScreen/SeeAndTypeScreen'
 
 function App() {
+
+  const [activeGameMode, setActiveGameMode] = useState<TGameMode | null>(null)
+
+  const changeActiveGameMode = (newGameMode: TGameMode) => {
+    setActiveGameMode(newGameMode)
+  }
+
+  const closeGameWindow = () => {
+    setActiveGameMode(null)
+  }
+
   return (
-    <>
-    </>
+    <div className={classes["app-background"]}>
+      <div className={classes["container"]}>
+        {activeGameMode || < StartScreen />}
+        {activeGameMode === "hearAndType" && <HearAndTypeScreen />}
+        {activeGameMode === "seeAndType" && <SeeAndTypeScreen />}
+      </div>
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  base: "/",
+  optimizeDeps: {
+    include: ['lodash']
+  },
+  server: {
+    host: '0.0.0.0'
+  }
+}) satisfies UserConfig
+
